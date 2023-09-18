80479dfa51922cd38cc5c6a444eb04b6f71655ac
Harshal <harshaltambat38@gmail.com>
2023-09-18 18:56:56 +0530

    springboot added

---
+++ src/test/java/com/example/ECommerce_website/ECommerceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ECommerce_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
