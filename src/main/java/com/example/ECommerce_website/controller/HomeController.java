d6349b35c3463ab6b98abf73eb7bacef265e4e39
Harshal <harshaltambat38@gmail.com>
2023-09-23 19:26:32 +0530

    shop page changes

--- src/main/java/com/example/ECommerce_website/controller/HomeController.java
+++ src/main/java/com/example/ECommerce_website/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -26,5 +27,20 @@ public class HomeController {
         model.addAttribute("categories", categoryService.getAllCategory());
         model.addAttribute("products", productService.getAllProducts());
         return "shop";
+    }
+
+    @GetMapping("/shop/category/{id}")
+    public String shopByCategory(Model model, @PathVariable int id)
+    {
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductsByCategoryId(id));
+        return "shop";
+    }
+
+    @GetMapping("/shop/viewproduct/{id}")
+    public String viewProductDetail(Model model, @PathVariable long id)
+    {
+        model.addAttribute("product", productService.getProductById(id).get());
+        return "viewProduct";
     }
 }
