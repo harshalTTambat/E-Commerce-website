d6627f5ecda61bd05e9c5dada028fbd23b220c73
Harshal <harshaltambat38@gmail.com>
2023-09-24 23:27:45 +0530

    security cofig started

---
+++ src/main/java/com/example/ECommerce_website/configuration/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.ECommerce_website.configuration;
+// from Spring boot 3.0.0 onwards
+// the WebSecurityConfigurerAdapter Class is removed/deprecated by Official spring boot
+// before spring boot 3.0.0 WebSecurityConfigurerAdapter class provide 3 methods for Authentication, HTTP, Authorization
+
+// Now this process is completely changed
+// we need to create @Been to Configure further
+
+public class SecurityConfig {
+}
